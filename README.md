fc7fa1d7b665e7018920906743defdee90d70d6f
Oj <oj@oojmed.com>
2020-03-19 07:45:30 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# OojChat-Client
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://choosealicense.com/licenses/mit/l)
+
+The web client for the new OojChat.
---
+++ app.js
@@ -0,0 +1,76 @@
+let socket = new WebSocket(`wss://vps.oojmed.com/chat/ws`);
+
+let chat = [];
+let username = 'unknown-user';
+
+function askUsername() {
+  username = prompt('Enter Username:');
+}
+
+function sendMsg(msg) {
+  socket.send(`send|${msg}`);
+}
+
+function UIMsg() {
+  sendMsg(document.getElementById('message-input').value);
+
+  document.getElementById('message-input').value = '';
+}
+
+function updateUI() {
+  for (let msg of chat) {
+    if (document.getElementById(`msg-${msg.id}`) === null) {
+      let el = document.createElement('div');
+      el.id = `msg-${msg.id}`;
+
+      el.className = msg.user === username ? 'msg-me' : 'msg-other';
+      el.classList.add('msg');
+
+      el.innerText = msg.msg;
+
+      el.setAttribute('data-username', msg.user);
+      
+      document.getElementById('chat').appendChild(el);
+    }
+  }
+}
+
+askUsername();
+
+socket.onopen = function(e) {
+  socket.send(`username|${username}`);
+};
+
+socket.onmessage = function(event) {
+  let split = event.data.split('|');
+  let type = split[0];
+  let data = split[1];
+
+  console.log(type, JSON.parse(data));
+
+  if (type === 'recv') {
+    chat.push(JSON.parse(data));
+
+    updateUI();
+  }
+
+  if (type === 'recap') {
+    chat = JSON.parse(data);
+
+    updateUI();
+  }
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    // e.g. server process killed or network down
+    // event.code is usually 1006 in this case
+    alert('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  console.error(error);
+};
